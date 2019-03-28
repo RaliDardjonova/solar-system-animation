c67ea06cd3901340eb177bdd433ef6fd3f740708
Ralitsa Dardjonova <r.dardjonova@gmail.com>
2019-03-28 17:06:02 +0200

    Added animation for the transfer mars-venus.
    Created animation for the transfer of mars to the orbit of venus.
    When the start button is clicked all sub orbits and scales of planets are
     cleard

--- scripts/animationScript.js
+++ scripts/animationScript.js
@@ -1,5 +1,6 @@
 //var k=0;
 //var v = 5;
+
 var EARTH_PERIOD = 10;
 var TYPE;
 var type;
@@ -19,11 +20,11 @@ timeSlider.on('change', function (e) {
   if(animateStar) animateStar(k++, v, sunData);
 });
 */
-var sceneView = $('#scene');
+var sceneView = $("#s");
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth*(3/4), window.innerHeight*(3/4));
 sceneView.append(renderer.domElement);
-renderer.setClearColor('dimgray');
+renderer.setClearColor("#000033");
 
 // включваме генерирането на меки сенки
 renderer.shadowMap.enabled = true;
@@ -37,7 +38,7 @@ camera.lookAt(new THREE.Vector3(0,0,0));
 
 //controling with the mouse
 
-var controls = new THREE.TrackballControls( camera );
+var controls = new THREE.TrackballControls( camera, renderer.domElement);
 //controls.addEventListener('change', render);
 //controls.target.set(0, 0, 0);
 /*
@@ -52,11 +53,6 @@ controls.dynamicDampingFactor = 0.3;
   //	var xp = 40*Math.random();
   //	var yp = 20+40*Math.random();
   //	var zp = 40*Math.random();
-
-var xp = 0;
-var yp = 0;
-var zp = 0;
-
 
   // създаване на топка
   /*
@@ -80,14 +76,24 @@ scene.add( ground );
 */
   // създаване на пилон
 var sun = new THREE.Mesh(
-  new THREE.SphereGeometry(6,32,32),
-  new THREE.MeshPhongMaterial({color:'yellow',shininess: 10, specular: 0xffffff})
+  new THREE.SphereGeometry(12,32,32),
+  new THREE.MeshPhongMaterial({color:'yellow',shininess: 100, specular: 0xffffff, emissive: 'yellow'})
 );
 
-sun.position.x = xp;
-sun.position.y = yp;
-sun.position.z = xp;
+sun.position.x = 0;
+sun.position.y = 0;
+sun.position.z = 0;
 scene.add( sun );
+
+var sun2 = new THREE.Mesh(
+  new THREE.SphereGeometry(14,32,32),
+  new THREE.MeshPhongMaterial({color:'yellow',shininess: 100, specular: 0xffffff, emissive: 'yellow',  transparent: true, opacity: 0.3  })
+);
+
+sun2.position.x = 0;
+sun2.position.y = 0;
+sun2.position.z = 0;
+scene.add( sun2 );
 
 /*
 var radius   = 30,
@@ -114,21 +120,21 @@ var mercuryGeo = mercuryPath.createPointsGeometry( 50 );
 
 mercuryEllipse = new THREE.Line(
     mercuryGeo,
-    new THREE.LineBasicMaterial( { color : 0xffaa00, linewidth : 2 } )
+    new THREE.LineBasicMaterial( { color : 'red', linewidth : 1 } ) // 0xffaa00
 );
 mercuryEllipse.color = 'orange';
 scene.add(mercuryEllipse);
 mercuryEllipse.rotation.set(Math.PI/2, 0, 0);
 
 var mercury = new THREE.Mesh(
-  new THREE.SphereGeometry(6,20,20),
+  new THREE.SphereGeometry(4,20,20),
   new THREE.MeshPhongMaterial({color:'orange',shininess: 10, specular: 0xffffff})
 );
 scene.add(mercury);
 //mercuryEllipse.position.set(11.91,0,0);
 
 //Venus
-var venusCurve = new THREE.EllipseCurve(0,0.73,108.2, 108.2,0,2*Math.PI,false);
+var venusCurve = new THREE.EllipseCurve(0,0 ,108.2, 108.2,0,2*Math.PI,false); // 0.73
 var venusPath = new THREE.Path( venusCurve.getPoints( 2000 ) );
 var venusGeo = venusPath.createPointsGeometry( 50 );
 
@@ -140,13 +146,13 @@ scene.add(venusEllipse);
 venusEllipse.rotation.set(Math.PI/2, 0, 0);
 
 var venus = new THREE.Mesh(
-  new THREE.SphereGeometry(2,20,20),
-  new THREE.MeshPhongMaterial({color:'green',shininess: 10, specular: 0xffffff})
+  new THREE.SphereGeometry(4,20,20),
+  new THREE.MeshPhongMaterial({color:'brown',shininess: 10, specular: 0xffffff})
 );
 scene.add(venus);
 
 //Earth
-var earthCurve = new THREE.EllipseCurve(0,-2.5,149.60, 149.58,0,2*Math.PI,false);
+var earthCurve = new THREE.EllipseCurve(0, 0,149.60, 149.58,0,2*Math.PI,false); // -2.5 149.58
 var earthPath = new THREE.Path( earthCurve.getPoints( 2000 ) );
 var earthGeo = earthPath.createPointsGeometry( 50 );
 
@@ -158,13 +164,13 @@ scene.add(earthEllipse);
 earthEllipse.rotation.set(Math.PI/2, 0, 0);
 
 var earth = new THREE.Mesh(
-  new THREE.SphereGeometry(6,20,20),
-  new THREE.MeshPhongMaterial({color:'green',shininess: 10, specular: 0xffffff})
+  new THREE.SphereGeometry(5,20,20),
+  new THREE.MeshPhongMaterial({color: 0x00b353,shininess: 10, specular: 0xffffff})
 );
 scene.add(earth);
 
 //Mars
-var marsCurve = new THREE.EllipseCurve(0,-21.3, 227.94, 226.94,0,2*Math.PI,false);
+var marsCurve = new THREE.EllipseCurve(0,0, 227.94, 226.94,0,2*Math.PI,false); //-21.3
 var marsPath = new THREE.Path( marsCurve.getPoints( 2000 ) );
 var marsGeo = marsPath.createPointsGeometry( 50 );
 
@@ -176,10 +182,28 @@ scene.add(marsEllipse);
 marsEllipse.rotation.set(Math.PI/2, 0, 0);
 
 var mars = new THREE.Mesh(
-  new THREE.SphereGeometry(6,32,32),
-  new THREE.MeshPhongMaterial({color:'green',shininess: 10, specular: 0xffffff})
+  new THREE.SphereGeometry(4,32,32),
+  new THREE.MeshPhongMaterial({color:'orange',shininess: 10, specular: 0xffffff})
 );
 scene.add(mars);
+
+// Ceres
+var ceresCurve = new THREE.EllipseCurve(0,0, 414.0869, 412.895595,0,2*Math.PI,false);
+var ceresPath = new THREE.Path( ceresCurve.getPoints( 2000 ) );
+var ceresGeo = ceresPath.createPointsGeometry( 50 );
+
+ceresEllipse = new THREE.Line(
+    ceresGeo,
+    new THREE.LineBasicMaterial( { color : 0xff0000 } )
+);
+scene.add(ceresEllipse);
+ceresEllipse.rotation.set(Math.PI/2, 0, 0);
+
+var ceres = new THREE.Mesh(
+  new THREE.SphereGeometry(4,32,32),
+  new THREE.MeshPhongMaterial({color:'grey',shininess: 10, specular: 0xffffff})
+);
+scene.add(ceres);
 
 //Jupiter
 var jupiterCurve = new THREE.EllipseCurve(-38.05, 0, 778.57, 777.5 ,0,2*Math.PI,false);
@@ -200,7 +224,7 @@ var jupiter = new THREE.Mesh(
 scene.add(jupiter);
 
 // Venus to Earth orbit
-var type2Curve = new THREE.EllipseCurve(0, 20.694471, 128.903398, 127.231383 ,0,2*Math.PI,false);
+var type2Curve = new THREE.EllipseCurve(0, 20.694471, 127.231383, 128.903398,0,2*Math.PI,false);
 var type2Path = new THREE.Path(type2Curve.getPoints(2000));
 var type2Geo = type2Path.createPointsGeometry( 50 );
 
@@ -208,8 +232,17 @@ type2Ellipse = new THREE.Line(
     type2Geo,
     new THREE.LineBasicMaterial( { color : 0xff00ff } )
 );
-scene.add(type2Ellipse);
-type2Ellipse.rotation.set(Math.PI/2, 0, 0);
+
+// Mars to Earth orbit
+var type3Curve = new THREE.EllipseCurve(0, -39.1612, 184.652613, 188.759601  ,0,2*Math.PI,false);
+var type3Path = new THREE.Path(type3Curve.getPoints(2000));
+var type3Geo = type3Path.createPointsGeometry( 50 );
+
+type3Ellipse = new THREE.Line(
+    type3Geo,
+    new THREE.LineBasicMaterial( { color : 'green'} )
+);
+
 
 // светлина
 var light = new THREE.PointLight();
@@ -258,10 +291,13 @@ function animate(){
   requestAnimationFrame(animate);
   var t = clock.getElapsedTime();
   var v;
-  var dif1, dif;
+  var dif1, dif, dif2, dif3, dif4;
   console.log("type: ");
   console.log(TYPE);
   if(TYPE == 2){
+    venus.scale.set(2.5, 2.5, 2.5);
+    scene.add(type2Ellipse);
+    type2Ellipse.rotation.set(Math.PI/2, 0, 0);
 
     if(t/(EARTH_PERIOD*0.615) > 0.75){
     //  console.log((t%(EARTH_PERIOD*0.79))/(EARTH_PERIOD*0.79));
@@ -284,10 +320,77 @@ function animate(){
     v = earthPath.getPoint((t%EARTH_PERIOD)/EARTH_PERIOD);
     earth.position.set(v.x, 0, v.y);
 
+    v = mercuryPath.getPoint((t%(EARTH_PERIOD*0.241))/(EARTH_PERIOD*0.241));
+    mercury.position.set(v.x, 0, v.y);
+
+    v = marsPath.getPoint((t%(EARTH_PERIOD*1.881))/(EARTH_PERIOD*1.881));
+    mars.position.set(v.x, 0, v.y);
+
+    v = ceresPath.getPoint((t%(EARTH_PERIOD*4.60))/(EARTH_PERIOD*4.60));
+    ceres.position.set(v.x, 0, v.y);
+
+    v = jupiterPath.getPoint((t%(EARTH_PERIOD*11.86))/(EARTH_PERIOD*11.86));
+    jupiter.position.set(v.x, 0, v.y);
 
   } else {
+    if(TYPE == 3){
+      mars.scale.set(2.5, 2.5, 2.5);
+      scene.add(type3Ellipse);
+      type3Ellipse.rotation.set(Math.PI/2, 0, 0);
+
+      scene.add(type2Ellipse);
+      type2Ellipse.rotation.set(Math.PI/2, 0, 0);
+
+      if(t/(EARTH_PERIOD*1.881) > 0.75){
+      //  console.log((t%(EARTH_PERIOD*0.79))/(EARTH_PERIOD*0.79));
+        dif1 = 0.75*1.881*EARTH_PERIOD;
+        if((t - dif1 + EARTH_PERIOD*0.75)/(EARTH_PERIOD) > 1.25 ){
+          dif2 = 1.25*EARTH_PERIOD + dif1 - EARTH_PERIOD;
+
+          if((t - dif2 + EARTH_PERIOD)/EARTH_PERIOD > 2.25){
+            dif3 = 2.25*EARTH_PERIOD + dif2 - EARTH_PERIOD;
+
+            if((t - dif3 + EARTH_PERIOD*0.25*0.79)/(EARTH_PERIOD*0.79) > 0.75){
+              dif4 = 0.75*EARTH_PERIOD*0.79 - EARTH_PERIOD*0.25*0.79 + dif3;
+              v = venusPath.getPoint(((t - dif4 + EARTH_PERIOD*0.75*0.615)%(EARTH_PERIOD*0.615))/(EARTH_PERIOD*0.615));
+              mars.position.set(v.x, 0, v.y);
+            } else {
+              v = type2Path.getPoint(((t - dif3 + EARTH_PERIOD*0.25*0.79)%(EARTH_PERIOD*0.79))/(EARTH_PERIOD*0.79));
+              mars.position.set(v.x, 0, v.y);
+            }
+          } else {
+            v = earthPath.getPoint(((t - dif2 + EARTH_PERIOD)%(EARTH_PERIOD))/(EARTH_PERIOD));
+            mars.position.set(v.x, 0, v.y);
+          }
+        } else {
+          //console.log(dif1);
+
+          v = type3Path.getPoint(((t - dif1 + EARTH_PERIOD*0.75)%(EARTH_PERIOD))/(EARTH_PERIOD));
+          mars.position.set(v.x, 0, v.y);
+        }
+      } else {
+        v = marsPath.getPoint((t%(EARTH_PERIOD*1.881))/(EARTH_PERIOD*1.881));
+        mars.position.set(v.x, 0, v.y);
+      }
+
+    } else {
+      v = venusPath.getPoint((t%(EARTH_PERIOD*0.615))/(EARTH_PERIOD*0.615));
+      venus.position.set(v.x, 0, v.y);
+    }
+    v = mercuryPath.getPoint((t%(EARTH_PERIOD*0.241))/(EARTH_PERIOD*0.241));
+    mercury.position.set(v.x, 0, v.y);
+
     v = venusPath.getPoint((t%(EARTH_PERIOD*0.615))/(EARTH_PERIOD*0.615));
     venus.position.set(v.x, 0, v.y);
+
+    v = earthPath.getPoint((t%EARTH_PERIOD)/EARTH_PERIOD);
+    earth.position.set(v.x, 0, v.y);
+
+    v = ceresPath.getPoint((t%(EARTH_PERIOD*4.60))/(EARTH_PERIOD*4.60));
+    ceres.position.set(v.x, 0, v.y);
+
+    v = jupiterPath.getPoint((t%(EARTH_PERIOD*11.86))/(EARTH_PERIOD*11.86));
+    jupiter.position.set(v.x, 0, v.y);
   }
   controls.update();
   renderer.render( scene, camera );
@@ -298,8 +401,13 @@ var isPaused;
 function Start(type)
 {
     TYPE = type;
-   console.log(type);
+    console.log(type);
     isPaused = false;
+    mars.scale.set(1, 1, 1);
+    venus.scale.set(1, 1, 1);
+    scene.remove(type2Ellipse);
+    scene.remove(type3Ellipse);
+    clock.start();
     animate();
 
 }
